b8b1bb633244bf927fcda3408f24733bded71561
jester <jaimebuddy98@gmail.com>
2022-05-10 17:53:35 -0500

    initial file setup

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+import Header from './components/Header';
+
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact'
+
+import Footer from './components/Footer'
+
+function App() {
+
+    //SET UP THE ABOUT TO BE RENDERED ON INITIAL LOAD 
+    const [currentPage, setCurrentPage] = useState('About');
+
+    //CONDTIONALLY RENDER EACH PAGE DEPENDING ON THE CURRENT-PAGE STATE
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Gallery') {
+            return <Gallery />;
+        }
+        return <Contact />;
+    };
+
+    // UPDATE DEPENDING ON PAGE
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    // ALWAYS RENDER THE HEADER, HERO, AND FOOTER, THEN USE THE RENDERPAGE FUNCTION TO INSERT THE BODY CONTENT
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* THE FUNCTIONS WILL BE PUSHED TO THE NAV COMPONENT */}
+                <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+            </header>
+            <main>
+                <Hero />
+
+                {renderPage()}
+
+                <Footer />
+            </main>
+        </div>
+    );
+}
+
+export default App;
