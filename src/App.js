7d48e7094c0e40eb83bfef32a5201da1a8bd52df
jester <jaimebuddy98@gmail.com>
2022-05-12 12:27:15 -0500

    worked on rendering

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact'
+import Home from './components/Home'
+// import About from './components/About';
+// import Gallery from './components/Gallery';
+// import Contact from './components/Contact'
 
-import Footer from './components/Footer'
+// import Footer from './components/Footer'
 
 function App() {
 
