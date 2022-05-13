548a31f882ac83f59ee50fd580f4016381c433b9
jester <jaimebuddy98@gmail.com>
2022-05-13 14:04:31 -0500

    added title and text plus simple styling

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import ('./style.css')
+import('./style.css')
 
 function Gallery() {
 
     return (
         <section id="gallery" className="gallery">
             <div className="container">
+
+                <div>
+                    <h2>Gallery</h2>
+                    <p>Donec eget risus a erat blandit sagittis. Nullam commodo lobortis ullamcorper. Phasellus ut bibendum erat. Curabitur nec felis ut augue interdum feugiat. Nunc vitae elit id nisi faucibus congue. Fusce vitae sem mi. </p>
+                    <p>Vestibulum accumsan eros ut accumsan maximus. Nunc dui est, dictum ut cursus at, faucibus non turpis. Sed mauris massa, faucibus elementum tempus fringilla, viverra id sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in eros laoreet, porttitor odio ac, porttitor metus. Pellentesque at tellus a velit maximus faucibus..</p>
+                </div>
+
 
 
             </div>
