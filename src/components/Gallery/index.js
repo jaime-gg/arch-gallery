77927c000362bc2ff53ca20afb5c1ef021ef0f7e
jester <jaimebuddy98@gmail.com>
2022-05-13 14:06:53 -0500

    added a repeated gallery png item

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -13,7 +13,39 @@ function Gallery() {
                     <p>Vestibulum accumsan eros ut accumsan maximus. Nunc dui est, dictum ut cursus at, faucibus non turpis. Sed mauris massa, faucibus elementum tempus fringilla, viverra id sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in eros laoreet, porttitor odio ac, porttitor metus. Pellentesque at tellus a velit maximus faucibus..</p>
                 </div>
 
+                <div className="row gallery-container" data-aos="fade-up" data-aos-delay="100">
 
+                    <div className="col-lg-4 col-md-6 gallery-item">
+                        <div className="gallery-wrap">
+                            <img src={require('../../Assets/gallery/gallery-1.png')} className="img-fluid" alt="" />
+                            <div className="gallery-links">
+                                <a href="https://github.com/jaime-gg/project1" title="View Github"><i className="bx bxl-github"></i></a>
+                                <a href="https://jaime-gg.github.io/project1/" title="View Page"><i className="bx bx-link"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 gallery-item">
+                        <div className="gallery-wrap">
+                            <img src={require('../../Assets/gallery/gallery-2.png')} className="img-fluid" alt="" />
+                            <div className="gallery-links">
+                                <a href="https://github.com/jaime-gg/tech-blog" title="View Github"><i className="bx bxl-github"></i></a>
+                                <a href="https://um-technically-blog.herokuapp.com/" title="View Page"><i className="bx bx-link"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-md-6 gallery-item">
+                        <div className="gallery-wrap">
+                            <img src={require('../../Assets/gallery/gallery-3.png')} className="img-fluid" alt="" />
+                            <div className="gallery-links">
+                                <a href="https://github.com/jaime-gg/storeEZ" title="View Github"><i className="bx bxl-github"></i></a>
+                                <a href="https://store-ez.herokuapp.com/" title="View Page"><i className="bx bx-link"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
         </section>
