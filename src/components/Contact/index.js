919502344dd785e57795a309f44d89d463c093c4
jester <jaimebuddy98@gmail.com>
2022-05-13 14:29:51 -0500

    basic contact page framework

---
+++ src/components/Contact/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+
+function Contact() {
+
+    return (
+        <section id="contact" className="container">
+            <div className="section-title">
+                <h2>Contact Me</h2>
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
