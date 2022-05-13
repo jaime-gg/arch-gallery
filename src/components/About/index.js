1eded8c533df8583a1fce018f1d8d66f51813d5a
jester <jaimebuddy98@gmail.com>
2022-05-13 13:52:49 -0500

    about page framework

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+
+    return (
+        <section id="about" className="about">
+            
+        </section>
+    )
+}
+
+export default About;
