a9feaf391af9c0a50b7fa6ac90ae932e82a7ad1c
jester <jaimebuddy98@gmail.com>
2022-05-13 13:58:05 -0500

    include new profile img

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,19 @@ function About() {
 
     return (
         <section id="about" className="about">
-            
+            <div className="container">
+
+                <div className="row">
+                    <h2>About</h2>
+                </div>
+
+                <div className="row">
+                    <div className="col-lg-4" data-aos="fade-right">
+                        <img src={require('../../Assets/profile-img.jpg')} className="img-fluid" alt="" />
+                    </div>
+                    
+                </div>
+            </div>
         </section>
     )
 }
