60cbe3b847a03e253d6d30a13a187ee657c33339
jester <jaimebuddy98@gmail.com>
2022-05-13 14:32:43 -0500

    contact page import + content

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -34,6 +34,32 @@ function Contact() {
             <div className="section-title">
                 <h2>Contact Me</h2>
             </div>
+
+            <div className="contact" data-aos="fade-in">
+                <form className="php-email-form">
+                    <div className="col-md-8 form-row justify-content-center">
+                        <div className="form-group">
+                            <label htmlFor="name">Your Name</label>
+                            <input onBlur={handleInputChange} defaultValue={name} type="text" name="name" className="form-control" id="name" />
+                        </div>
+                        <div className="form-group ">
+                            <label htmlFor="name">Your Email</label>
+                            <input onBlur={handleInputChange} defaultValue={email} type="email" className="form-control" name="email" id="email" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="name">Message</label>
+                            <textarea onBlur={handleInputChange} defaultValue={message} className="form-control" name="message" rows="8" ></textarea>
+                        </div>
+
+                        <div className="text-center"><button type="submit" onClick={handleFormSubmit}>Send Message</button></div>
+                    </div>
+                </form>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+            </div>
         </section>
     )
 }
