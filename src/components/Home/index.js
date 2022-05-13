4f6f53177a82d69d38bd6f1d34e8ec3dd7a5cd82
jester <jaimebuddy98@gmail.com>
2022-05-13 15:24:02 -0500

    needs div size adjustment for bg img [bug]

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Require} from 'react';
 import './style.css'
 
 function Home({ currentPage, handlePageChange }) {
@@ -7,20 +7,20 @@ function Home({ currentPage, handlePageChange }) {
         <section>
             <div className='justify-content-center'>
                 <h3 className="text-center">Choose Your Path: </h3>
-                <div className='home-arch row'>
-                    <div className='arch col p-4'>
+                <div className='home-arch row h-100'>
+                    <div className='arch col p-4 d-flex justify-content-center text-center'>
                         <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'active nav-link ' : 'nav-link '} >
-                            <h3 className="center-text">About Me</h3>
+                            <h3 className="justify-content-center">About Me</h3>
                         </a>
                     </div>
 
-                    <div className='arch col p-4'>
+                    <div className='arch col p-4 d-flex justify-content-center text-center'>
                         <a href="#gallery" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Gallery' ? 'active nav-link ' : 'nav-link '}>
                             <h3 className="">Gallery</h3>
                         </a>
                     </div>
 
-                    <div className='arch col p-4'>
+                    <div className='arch col p-4 d-flex justify-content-center text-center'>
                         <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'active nav-link ' : 'nav-link '}>
                             <h3 className="">Contact Me</h3>
                         </a>
