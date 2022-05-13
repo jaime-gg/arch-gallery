726b76ba7544930b1b0b8aa9d3ad2651ee6ab952
jester <jaimebuddy98@gmail.com>
2022-05-13 14:01:06 -0500

    gallery framework

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ('./style.css')
+
+function Gallery() {
+
+    return (
+        <section id="gallery" className="gallery">
+            <div className="container">
+
+
+            </div>
+        </section>
+    )
+}
+
+export default Gallery;
