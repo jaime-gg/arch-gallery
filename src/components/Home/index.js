257fa4eae8b8a82315bb47fe5e400bf7c964fda0
jester <jaimebuddy98@gmail.com>
2022-05-14 17:19:38 -0500

    included img file w/ no visibility

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
-import React, {Require} from 'react';
+import React, { Require } from 'react';
 import './style.css'
+import archImg from '../../Assets/arch.png'
 
 function Home({ currentPage, handlePageChange }) {
 
@@ -9,20 +10,23 @@ function Home({ currentPage, handlePageChange }) {
                 <h3 className="text-center">Choose Your Path: </h3>
                 <div className='home-arch row h-100'>
                     <div className='arch col p-4 d-flex justify-content-center text-center'>
+                        <img src={archImg} />
                         <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'active nav-link ' : 'nav-link '} >
-                            <h3 className="justify-content-center">About Me</h3>
+                            <h3 className="main-title">About Me</h3>
                         </a>
                     </div>
 
                     <div className='arch col p-4 d-flex justify-content-center text-center'>
+                        <img src={archImg} />
                         <a href="#gallery" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Gallery' ? 'active nav-link ' : 'nav-link '}>
-                            <h3 className="">Gallery</h3>
+                            <h3 className="main-title">Gallery</h3>
                         </a>
                     </div>
 
                     <div className='arch col p-4 d-flex justify-content-center text-center'>
+                        <img src={archImg} />
                         <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'active nav-link ' : 'nav-link '}>
-                            <h3 className="">Contact Me</h3>
+                            <h3 className="main-title">Contact Me</h3>
                         </a>
                     </div>
                 </div>
