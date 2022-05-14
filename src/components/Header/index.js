821b3bf1b1dc1be8ec33d81c882862dbe3344cab
jester <jaimebuddy98@gmail.com>
2022-05-14 17:39:24 -0500

    added img to nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import './style.css';
+import archImg from '../../Assets/arch.png'
 
 // PULL IN THE NESSESSARY PROPS
 function Header({ currentPage, handlePageChange }) {
     const renderNav = () => {
         if (currentPage === 'Home') {
-            return <h3> THE ARCH GALLERY BIDS YOU WELCOME</h3>;
+            return (
+                <div>
+                    <img src={archImg} />
+                    <h3> THE ARCH GALLERY BIDS YOU WELCOME</h3>
+                </div>
+            );
         }
         return (
-            <ul className="justify-content-center">
-                <li>
-                    <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'active nav-link ' : 'nav-link '} >
-                        <span>About Me</span>
-                    </a>
-                </li>
+            <div>
+                <img src={archImg} />
+                <ul className="justify-content-center">
+                    <li>
+                        <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'active nav-link ' : 'nav-link '} >
+                            <span>About Me</span>
+                        </a>
+                    </li>
 
-                <li>
-                    <a href="#gallery" onClick={() => handlePageChange('Gallery')} className={currentPage === 'Gallery' ? 'active nav-link ' : 'nav-link '}>
-                        <span>Gallery</span>
-                    </a>
-                </li>
+                    <li>
+                        <a href="#gallery" onClick={() => handlePageChange('Gallery')} className={currentPage === 'Gallery' ? 'active nav-link ' : 'nav-link '}>
+                            <span>Gallery</span>
+                        </a>
+                    </li>
 
-                <li>
-                    <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'active nav-link ' : 'nav-link '}>
-                        <span>Contact</span>
-                    </a>
-                </li>
-            </ul>
+                    <li>
+                        <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'active nav-link ' : 'nav-link '}>
+                            <span>Contact</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         );
     };
 
